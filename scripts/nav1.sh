d3217b7507fe1a345d2b2788dd76aa5392d84e6c
whitbrunn <whitbrun1010@gmail.com>
2025-04-06 03:19:31 +0800

    0406: Good D* and PID Controller

---
+++ scripts/nav1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+
+
+gnome-terminal --tab -- bash -c "
+  cd /home/jy/fun_project;
+  roslaunch me5413_world world.launch;
+  exec bash
+"
+
+
+sleep 3
+
+
+
+# Launch cartographer in a new terminal
+gnome-terminal --tab -- bash -c "
+  cd /home/jy/fun_project;
+  source /home/jy/fun_project/ME5413_Final_Project/src/third_party/ros_motion_planning/devel/setup.bash;
+  roslaunch jackal_navigation navigation1.launch;
+  exec bash
+"
+echo "Running"
